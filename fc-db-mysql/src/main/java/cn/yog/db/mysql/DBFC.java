745836f87c8389e943143f3d69ac6355a36ca346
matthew.miao <matthew.miao@yunlsp.com>
2021-01-23 15:19:15 +0800

    sfc

--- fc-db-mysql/src/main/java/cn/yog/db/mysql/DBFC.java
+++ fc-db-mysql/src/main/java/cn/yog/db/mysql/DBFC.java
@@ -2,7 +2,6 @@ package cn.yog.db.mysql;
 
 import com.aliyun.fc.runtime.Context;
 import com.aliyun.fc.runtime.FunctionComputeLogger;
-import com.aliyun.fc.runtime.FunctionInitializer;
 import com.aliyun.fc.runtime.StreamRequestHandler;
 
 import java.io.IOException;
@@ -10,8 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.*;
 
-public class DBFC implements StreamRequestHandler, FunctionInitializer {
-
+public class DBFC implements StreamRequestHandler {
   private String host = System.getenv("MYSQL_HOST");
   private String port = System.getenv("MYSQL_PORT");
   private String dbName = System.getenv("MYSQL_DBNAME");
@@ -22,11 +20,6 @@ public class DBFC implements StreamRequestHandler, FunctionInitializer {
 
   {
     url = String.format("jdbc:mysql://%s:%s/%s?useSSL=false", host, port, dbName);
-  }
-
-  @Override
-  public void initialize(Context context) {
-    context.getLogger().info(context.getRequestId());
   }
 
   @Override
@@ -48,7 +41,7 @@ public class DBFC implements StreamRequestHandler, FunctionInitializer {
 
       logger.info("Successfully executed query.  Current: " + currentTime);
 
-      String sql = "INSERT INTO user (id, name) VALUES(?, ?)";
+      String sql = "REPLACE INTO users (id, name) VALUES(?, ?)";
 
       PreparedStatement ps = conn.prepareStatement(sql);
       ps.setString(1, "3");
@@ -56,7 +49,7 @@ public class DBFC implements StreamRequestHandler, FunctionInitializer {
 
       ps.execute();
 
-      resultSet = stmt.executeQuery("SELECT * FROM user");
+      resultSet = stmt.executeQuery("SELECT * FROM users");
 
       while (resultSet.next()) {
         logger.info("user: " + resultSet.getString(2));
